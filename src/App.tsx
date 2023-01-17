5ca10bd6549460c6df7832c297e8af49ea78a71e
Nuntapong <nuntapong@odds.team>
2023-01-17 13:51:47 +0700

    add: who am i components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="portfolio-body">eiei</div>;
+  return (
+    <div>
+      <>
+        <h1>Who Am I?</h1>
+      </>
+    </div>
+  );
 }
 
 export default App;
