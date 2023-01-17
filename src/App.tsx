571cb9c547a24fdc2874b3fcb668c2c71fbdb5ed
Nuntapong <nuntapong@odds.team>
2023-01-17 14:10:44 +0700

    add: who am i on landing1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import WhoAmI from "./components/whoAmI";
 
 function App() {
   return (
     <div>
       <>
-        <h1>Who Am I?</h1>
+        <WhoAmI />
       </>
     </div>
   );
---
+++ src/components/whoAmI.tsx
@@ -0,0 +1,15 @@
+import "../styles/portfolio.css";
+
+const WhoAmI = () => {
+  return (
+    <>
+      <div className="portfolio-body">
+        <div className="landing1">
+          <div className="text-content">Nuntapong Siripanyawong</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WhoAmI;
