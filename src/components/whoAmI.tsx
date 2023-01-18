4773e55c4abb88573c8fcb118ed91b1932a3631a
Nuntapong <nuntapong@odds.team>
2023-01-18 08:57:11 +0700

    edit: use interface IMyIdentity

--- src/components/whoAmI.tsx
+++ src/components/whoAmI.tsx
@@ -1,11 +1,17 @@
+import identity from "../lang/word.json";
 import "../styles/portfolio.css";
 
+export interface IMyIdentity {
+  myName: string;
+}
+
 const WhoAmI = () => {
+  const myIdentity: IMyIdentity = identity;
   return (
     <>
       <div className="portfolio-body">
         <div className="landing1">
-          <div className="text-content">Nuntapong Siripanyawong</div>
+          <div className="title-name">{myIdentity.myName}</div>
         </div>
       </div>
     </>
